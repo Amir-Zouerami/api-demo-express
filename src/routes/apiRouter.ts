a105e32d2f9a9e21e0b9d8209e9af13baa18bf93
amir.zouerami <amir.zouerami@partsoftware.com>
2024-01-27 15:51:23 +0330

    new `users` route for endpoints relating to users (for now only has the `editUser` sub-route).

--- src/routes/apiRouter.ts
+++ src/routes/apiRouter.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { login } from "./auth/login";
 import { signupRouter } from "./auth/signup";
+import { editUser } from "./user/editUser";
 
 const apiRouter = express.Router();
 
 apiRouter.use("/auth", login, signupRouter);
+
+apiRouter.use("/users", editUser);
 
 export default apiRouter;
