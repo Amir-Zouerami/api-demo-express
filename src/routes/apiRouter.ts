610122c7af7d0a918d7505201ea6945e84db43ac
amir.zouerami <amir.zouerami@partsoftware.com>
2024-01-27 17:54:24 +0330

    changed `users` path to `user`.

--- src/routes/apiRouter.ts
+++ src/routes/apiRouter.ts
@@ -2,11 +2,12 @@ import express from "express";
 import { login } from "./auth/login";
 import { signupRouter } from "./auth/signup";
 import { editUser } from "./user/editUser";
+import { generateCaptchaMiddlewar } from "../utility/captcha";
 
 const apiRouter = express.Router();
 
 apiRouter.use("/auth", login, signupRouter);
 
-apiRouter.use("/users", editUser);
+apiRouter.use("/user", editUser);
 
 export default apiRouter;
