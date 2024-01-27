23e4cfd3981ff5f0824e9f73fdc49631443f8374
amir.zouerami <amir.zouerami@partsoftware.com>
2024-01-27 17:54:44 +0330

    defines the `captcha` endpoint to generate and give a captcha to.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,27 @@
-import "dotenv/config";
-import express from "express";
+import { generateCaptcha } from "./utility/captcha";
+import { CustomSessionData } from "./types/Session";
 import apiRouter from "./routes/apiRouter";
+import session from "express-session";
 import bodyParser from "body-parser";
+import express from "express";
+import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
+app.use(
+  session({
+    secret: process.env.session_secret as string,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-app.get("/test", (req, res) => {
-  res.send("Service is up and running...!");
+app.get("/captcha", (req, res) => {
+  const captcha = generateCaptcha();
+  (req.session as CustomSessionData).captcha = captcha;
+  res.send(captcha);
 });
 
 app.use("/api", apiRouter);
